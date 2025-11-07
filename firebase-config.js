1ade46a3a807da4c2efbf872936b6f5e93a6bbfb
Nowiexx <noelbellotindos0@gmail.com>
2025-11-07 16:53:45 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1dPyr7lwob9N4C6PnNtQ_cpQVj5amTxo",
+  authDomain: "iskedmathay-1d606.firebaseapp.com",
+  projectId: "iskedmathay-1d606",
+  storageBucket: "iskedmathay-1d606.appspot.com",
+  messagingSenderId: "575254393281",
+  appId: "1:575254393281:web:d41f802de78b506b373f40",
+  measurementId: "G-2JB8ZXDZ51"
+};
+
